c4b5260233d8ebfb9ad64ebfd99bc5207d224906
Subhadip Ghorai <subhadipghorai92@gmail.com>
2024-12-31 15:17:29 +0530

    Exercise 9

---
+++ video_59/index.js
@@ -0,0 +1,15 @@
+//Exercises 9
+
+/*
+    Create a faulty calculator using javascript
+
+    this faulty calculator dose following :
+1.  it takes two number as input form user 
+2. it performs wrong operation as following:
+        + ---> -
+        * ---> +
+        - ---> /
+        / ---> **
+it performs wrong operation 10% of the times
+
+*/
