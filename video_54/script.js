9d8602b96a85b03dd97ebd97226fa2222455a212
Subhadip Ghorai <subhadipghorai92@gmail.com>
2024-12-31 09:12:16 +0530

    introduction to the javascript

---
+++ video_54/script.js
@@ -0,0 +1,20 @@
+alert("Hello world!");
+
+console.log("Code is running");
+console.log("Code is  also running");
+console.log("Code is running in chrome");
+
+var a =prompt("Enter your number")
+var isTrue =confirm("Are you sure you want to leave this page and burn your computer ")
+if(isTrue){
+    console.log("computer is burn")
+}
+else{
+    console.log("computer is not burn")
+}
+
+console.log("your number is "+a)
+
+document.title="hey i am good"
+
+// document.body.style.backgroundColor="red"
