376529149f5866bf68c8fe4aa15e84522a8729db
Subhadip Ghorai <subhadipghorai92@gmail.com>
2024-12-31 14:48:07 +0530

    loop concept of javascript

---
+++ video_57/index.js
@@ -0,0 +1,43 @@
+console.log("Tutorial on javascript loops")
+
+// let a=1;
+// for (let i = 0; i < 50; i++) {
+//         console.log(a+i);
+    
+// } 
+
+// //for in loop
+// let obj ={
+//     "name": "Subhadip",
+//     "role":"Developer",
+//     "company":"TCS",
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key,element);
+// }
+
+//for of loop
+
+// for (const c of "objects") {
+//         console.log(c);
+    
+// }
+
+//while loop
+
+// let i=1;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+//do while loop
+
+let i=0;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
+
