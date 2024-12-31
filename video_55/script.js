29c35d76e7c6a36f0861b2e86d802db4835f98c3
Subhadip Ghorai <subhadipghorai92@gmail.com>
2024-12-31 09:43:26 +0530

    javascript variable and the data types

---
+++ video_55/script.js
@@ -0,0 +1,40 @@
+console.log("Hey this is tutorial for javascript variables and data types ")
+
+let a=10;
+// a=a+3;
+let b=23;
+let c="Subhadip";
+// console.log(typeof a, typeof b, typeof c)
+// console.log(a+b+7)
+{
+    let a=78;
+    console.log(a)
+}
+console.log(a)
+
+
+const a1=5;
+// const a1=a1+1;
+// not allowed because a1 is a constant
+
+let x = " Subha bhai";
+let y =22;
+let z= 3.55;
+const p =true;
+let q= undefined;
+let r = null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name": "Subha",
+    "job role": "developer",
+    "age": 21,
+    "is handsome":true
+
+}
+console.log(o)
+o.salary = "1000crores"
+console.log(o)
+o.salary = "5000crores"
+console.log(o)
