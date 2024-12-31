8a781ffac91c62eea30fc767cfeca59dd667cdbd
Subhadip Ghorai <subhadipghorai92@gmail.com>
2025-01-01 04:58:05 +0530

    string in javascript

---
+++ video_60/index.js
@@ -0,0 +1,27 @@
+//javascript String 
+
+let a = "Subhadip";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+
+console.log(a.length);
+
+let name="Ram"
+let friend ="Rohan"
+console.log("His name is "+name + " and his friend name is "+friend)
+console.log(`his name is ${name} and his friend name is ${friend}`)
+
+let b= "Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(0,4))
+console.log(b.replace("Sh","77"))
+
+console.log(b.concat(a))
