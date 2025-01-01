806234ac4b929e3074a4b87fe190181f0fb00ae1
Subhadip Ghorai <subhadipghorai92@gmail.com>
2025-01-02 02:52:21 +0530

    Exercise 10 problem

---
+++ video_62/index.js
@@ -0,0 +1,21 @@
+//Exercises 10 
+
+/*
+create a business name generator by combining list of adjectives and shop name and another word
+
+Adjectives:
+
+crazy
+Amazing
+Fire
+
+shop Name:
+Engine
+Foods
+Garments
+
+Another word :
+Bros
+Limited
+Hub
+*/
