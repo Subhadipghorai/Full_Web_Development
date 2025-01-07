7dc657694c8e18217c8831fa652cc934466cc0f7
Subhadip Ghorai <subhadipghorai92@gmail.com>
2025-01-07 15:04:45 +0530

    javascript-DOM- children,parent,sibling nodes

---
+++ video_67/script.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+document.body
+
+document.body.childNodes
+
+document.body.childNodes[0]
+
+document.body.childNodes[1]
+
+document.body.childNodes[1].childNodes
+
+let cont =document.body.childNodes[1]
+
+cont.firstChild
+
+cont.firstElementChild
+
+document.body.firstElementChild
+
+document.body.firstElementChild.childNodes
+
+document.body.firstElementChild.children
