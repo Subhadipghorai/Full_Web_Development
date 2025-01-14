5031f0a1d40abbc9cd54609852946d5baa464108
Subhadip Ghorai <subhadipghorai92@gmail.com>
2025-01-14 18:51:01 +0530

    async function get and post request await and fetch api

---
+++ video_76/script.js
@@ -0,0 +1,48 @@
+// function getData(){
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(455)
+
+//         }, 3500);
+
+//     })
+
+// }
+async function getData() {
+    // let x= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let x = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            title: "foo",
+            body: "bar",
+            userId: 1,
+        }),
+        headers: {
+            "content-type": "application/json; charset=UTF-8",
+        },
+    });
+    let data = await x.json();
+    return data;
+}
+async function main() {
+    console.log("Loading Modules ");
+
+    console.log("do something else ");
+
+    console.log("load data");
+
+    let data = await getData();
+
+    console.log(data);
+
+    console.log("process data");
+
+    console.log("task 2");
+}
+main();
+
+// data.then( (v) => {
+//     console.log(data)
+//     console.log("process data")
+//     console.log("task 2")
+// })
