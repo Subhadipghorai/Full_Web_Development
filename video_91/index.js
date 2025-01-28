a70ed4de046397ff818e76f000f226bdef118a5b
Subhadip Ghorai <subhadipghorai92@gmail.com>
2025-01-28 15:14:43 +0530

    exercise 15 problem statement

---
+++ video_91/index.js
@@ -0,0 +1,11 @@
+// You have to write a Node.js progrm to clear clutter inside a directory and organize the contents of that directory into different folders.
+
+// For example this file become
+//  1. name.jpg 
+// 2. name.png
+// 3. this.pdf
+// 4. abc.zip
+// 5. xyz.rar
+// 6. cat.jpeg
+
+// this:
