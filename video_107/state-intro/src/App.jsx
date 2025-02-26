ad09f346ebabb6475bd4ef20b1abb0eae207b511
Subhadip Ghorai <subhadipghorai92@gmail.com>
2025-02-26 07:40:26 +0530

    The concept of React Hooks and State

---
+++ video_107/state-intro/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>the count is{count}</div>
+      <button onClick={()=>setCount(count+1)}>Update Count</button>
+    </>
+  )
+}
+
+export default App
